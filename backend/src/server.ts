3f43da98d0deca277e7e631189106b6f6bf4b5f6
vitor <vitor.campos@autaza.com>
2024-11-27 21:52:16 -0300

    clean comments

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,6 @@
 import { App } from './app';
 import 'dotenv/config';
 
-// import dotenv from 'dotenv';
-// import path from 'path';
-
-// dotenv.config({ path: path.resolve(__dirname, '../../.env') }); 
-
-console.log(process.env.APP_PORT)
-
 const PORT = process.env.APP_PORT || 8080;
 
 new App().start(PORT);
